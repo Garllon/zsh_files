ebd583181892d677d557c0d9aea6b977b9e53109
garllon <palluthe.bennet@gmail.com>
2016-12-23 15:42:23 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ZSH_FILES=$PWD
+
+curl -L http://install.ohmyz.sh | sh
+rm $HOME/.zshrc 2>/dev/null
+rm $HOME/.zshrc-e 2>/dev/null
+
+ln -s $ZSH_FILES/zshrc $HOME/.zshrc
+
+chsh -s /bin/zsh
+/bin/zsh
+source $HOME/.zshrc
